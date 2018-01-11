a93c9147e9d8bf79c129df50e0b50bdda35f0c7d
Aleksey Suleymanov <aleksey.suleymanov@gmail.com>
2018-01-11 12:52:56 +0000

    edit function

--- client/verify.go
+++ client/verify.go
@@ -118,15 +118,15 @@ func (api *Client) Verify_Post(author, permlink string) bool {
 	}
 }
 
-func (api *Client) Verify_Delegate_Posting_Key_Sign(from, to string) bool {
-	acc, err := api.Rpc.Database.GetAccounts([]string{from})
+func (api *Client) Verify_Delegate_Posting_Key_Sign(from_user, to_user string) bool {
+	acc, err := api.Rpc.Database.GetAccounts([]string{from_user})
 	if err != nil {
 		log.Println(errors.Wrapf(err, "Error Verify Delegate Vote Sign: "))
 		return false
 	} else if len(acc) == 1 {
 		for _, v := range acc[0].Posting.AccountAuths {
 			tu := strings.Split(strings.Replace(strings.Replace(fmt.Sprintf("%v", v), "[", "", -1), "]", "", -1), " ")
-			if tu[0] == to {
+			if tu[0] == to_user {
 				if tu[1] == fmt.Sprintf("%v", acc[0].Posting.WeightThreshold) {
 					return true
 				}

13ccb6d43b6c194dbbc74864e76e361cdccbcd87
Andrey Velikoredchanin <andy@andyhost.ru>
2018-06-17 12:42:34 +0300

    Add options in SendTrx for async transactions send posibility.

--- trx.go
+++ trx.go
@@ -1,13 +1,19 @@
 package client
 
 import (
+	"github.com/asuleymanov/golos-go/apis/network_broadcast"
 	"github.com/asuleymanov/golos-go/transactions"
 	"github.com/asuleymanov/golos-go/types"
 	"time"
 )
 
 //SendTrx generates and sends an array of transactions to GOLOS.
-func (client *Client) SendTrx(username string, strx []types.Operation) (*BResp, error) {
+func (client *Client) SendTrx(username string, strx []types.Operation, opts ...map[string]interface{}) (*BResp, error) {
+	var options map[string]interface{}
+	if len(opts) > 0 {
+		options = opts[0]
+	}
+
 	// Получение необходимых параметров
 	props, err := client.Database.GetDynamicGlobalProperties()
 	if err != nil {
@@ -47,17 +53,26 @@ func (client *Client) SendTrx(username string, strx []types.Operation) (*BResp, 
 	}
 
 	// Отправка транзакции
-	resp, err := client.NetworkBroadcast.BroadcastTransactionSynchronous(tx.Transaction)
+	var resp *network_broadcast.BroadcastResponse
+	if options != nil && options["async"].(bool) {
+		err = client.NetworkBroadcast.BroadcastTransaction(tx.Transaction)
+	} else {
+		resp, err = client.NetworkBroadcast.BroadcastTransactionSynchronous(tx.Transaction)
+	}
 
 	if err != nil {
 		return nil, err
 	}
-	var bresp BResp
+	if resp != nil {
+		var bresp BResp
 
-	bresp.ID = resp.ID
-	bresp.BlockNum = resp.BlockNum
-	bresp.TrxNum = resp.TrxNum
-	bresp.Expired = resp.Expired
+		bresp.ID = resp.ID
+		bresp.BlockNum = resp.BlockNum
+		bresp.TrxNum = resp.TrxNum
+		bresp.Expired = resp.Expired
 
-	return &bresp, nil
+		return &bresp, nil
+	}
+
+	return nil, nil
 }

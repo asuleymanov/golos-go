112a218c99feeb10ba8533db5b159376e17059f7
Aleksey Suleymanov <Aleksey.Suleymanov@gmail.com>
2018-08-01 13:55:53 +0000

    Update proposal_operation.go

--- types/proposal_operation.go
+++ types/proposal_operation.go
@@ -1,3 +1,32 @@
 package types
 
-type ProposalOperations []interface{}
+import (
+	"encoding/json"
+)
+
+type ProposalOperation struct {
+    Operation Operation `json:"op"`
+    OperationType          OpType    `json:"-"`
+}
+
+type rawProposalOperation struct {
+	Operation              *operationTuple `json:"op"`
+}
+
+func (op *ProposalOperation) UnmarshalJSON(p []byte) error {
+	var raw rawOperationObject
+	if err := json.Unmarshal(p, &raw); err != nil {
+		return err
+	}
+
+	op.Operation = raw.Operation.Data
+	op.OperationType = raw.Operation.Type
+	return nil
+}
+
+
+func (op *ProposalOperation) MarshalJSON() ([]byte, error) {
+	return JSONMarshal(&rawOperationObject{
+		Operation:              &operationTuple{op.Operation.Type(), op.Operation},
+	})
+}
